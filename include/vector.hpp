bb6f542450fe2729535a0aacbcc80f58dd9ac12b
cfrancoi <corentinfrancois31@gmail.com>
2021-07-02 17:53:20 +0200

    vector update

--- include/vector.hpp
+++ include/vector.hpp
@@ -19,6 +19,7 @@ namespace ft
 				typedef _Self			iterator_type;
 				typedef std::random_access_iterator_tag iterator_category;
 				typedef std::ptrdiff_t difference_type;
+				typedef size_t			size_type;
 			
 			Random_acc_iter() : _add(NULL) {}
 
@@ -33,7 +34,7 @@ namespace ft
 			reference	operator*() { return *_add; }
 			pointer		operator->() { return _add; }
 
-			//reference operator[]();
+			reference operator[](size_type n) { return *(_add + n); }
 			//const_reference operator[](size_type n) const;
 
 			_Self& operator=(const _Self & ref)
@@ -42,6 +43,8 @@ namespace ft
 				return *this;
 			}
 
+			friend _Self& operator+(const size_type & i) { _add += i; return *this; }
+            
 		/*	_Self& operator=(const _List_Iterator<_Tp> & ref)
 			{
 				return *this;
@@ -121,6 +124,62 @@ namespace ft
 			return ret;
 		}
 
+		void		del_block(pointer to_del, size_type size)
+		{
+			_alloc.deallocate(to_del, size);
+		}
+
+		void		add_mem(size_type n, const value_type & val)
+		{
+			pointer new_v;
+
+			new_v = new_block(size() + n); // check fail alloc
+
+			iterator it = begin();
+			size_type n_size = size() + n;
+			size_type p_size = size();
+			size_type i = 0;
+
+			while (i != p_size)
+			{
+				new_v[i] = it[i];
+				++i;
+			}
+			while (i != n_size)
+			{
+				new_v[i] = val;
+				++i;
+			}
+
+			pointer cpy = _start;
+			_start = new_v;
+			_end = new_v + n_size;
+			_end_of_storage = _end;
+			del_block(cpy, p_size);
+		}
+		void	add_size(size_type n, const value_type & val)
+		{
+			size_type i;
+
+			i = 0;
+			while (i < n && _end <= _end_of_storage)
+			{
+				*++_end = val;
+				++i;
+			}
+		}
+		void	rm_size(size_type n)
+		{
+			size_type i;
+
+			i = 0;
+			while (i < n && _end >= _start)
+			{
+				--_end;
+				++i;
+			}
+		}
+
 		/*
 			** Constructor && Destructor **
 		*/
@@ -136,7 +195,7 @@ namespace ft
 				const value_type& val,
 				const allocator_type& alloc = allocator_type()) : _start(NULL), _end(NULL), _end_of_storage(NULL), _alloc(alloc)
 		{
-
+			assign(n, val);
 		}
 
 		// Range Constructor
@@ -161,7 +220,7 @@ namespace ft
 		iterator begin() {	return _start; }
 		const_iterator begin() const;
 
-		iterator end();
+		iterator end() { return _end; }
 		const_iterator end() const;
 
 		reverse_iterator rbegin();
@@ -175,13 +234,13 @@ namespace ft
 		*/
 
 		// return actual size
-		size_type size(void) const;
+		size_type size(void) const { return static_cast<size_type>(_end - _start); }
 
 		// return max size
 		size_type max_size(void) const;
 
 		// return allocated size
-		size_type capacity(void) const;
+		size_type capacity(void) const { return static_cast<size_type>(_end_of_storage - _start); };
 
 		bool empty() const;
 
@@ -192,11 +251,11 @@ namespace ft
 			*** Access Function ***
 		*/
 
-		reference operator[](size_type n);
+		reference operator[](size_type n) { return this->at(n); }
 
 		const_reference operator[](size_type n) const;
 
-		reference at(size_type n);
+		reference at(size_type n) { return *(_start + n); }
 		
 		const_reference at(size_type n) const;
 
@@ -212,22 +271,54 @@ namespace ft
 			*** Modifier Function ***
 		*/
 
-		
-
 		void assign (size_type n, const value_type& val)
 		{
+			if (n == 0)
+			return ;
+			if (n + size() > capacity())
+				add_mem((n - size()), val);
+			else
+				add_size(n, val);
+		}
+
+		template <class Ite>
+		void assign (Ite f, Ite l)
+		{
+			while (f != l)
+			{
+				assign(1, *f++);
+			}
+		}
+
+		void resize (size_type n, const value_type & val = value_type())
+		{
+			if (n == size())
+				return ;
+			else if (n < size())
+			{
+				rm_size(size() - n);
+			}
+			else if (n > size())
+			{
+				if (n > capacity())
+					add_mem((n - size()), val);
+				else
+					add_size(n - size(), val);
+			}
 
 		}
 
-		//template <class Ite>
-		//void assign (Ite f, Ite l);
+		void push_back(const value_type & val)
+		{
+			assign(1, val);
+		}
 
-
-		void resize (size_type n, const value_type & val = value_type());
-
-
-
-
+		void pop_back(void)
+		{
+			if (size())
+				rm_size(1);
+		}
+		
 	};
 	
 } // namespace ft
