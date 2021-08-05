38de3729e0c1787433c7e09b46eee397fffbc58a
cfrancoi <corentinfrancois31@gmail.com>
2021-08-05 11:56:03 +0200

    all prototype

--- include/map.hpp
+++ include/map.hpp
@@ -3,10 +3,19 @@
 # include <functional>
 # include <utility>
 # include <memory>
+# include <iterator.hpp>
 
 namespace ft
 {
-	template < class Key, class T, class Compare = std::less<Key>, class alloc = std::allocator< std::pair<const Key, T> > >
+	template < class _Tp >
+	class const_map_iterator;
+
+	template < class _Tp >
+	class map_iterator;
+
+
+
+	template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator< std::pair<const Key, T> > >
 	class map
 	{
 		public:
@@ -16,17 +25,18 @@ namespace ft
 			typedef T		mapped_type;
 			typedef std::pair<const Key, T> value_type;
 			typedef Compare			key_compare;
+			typedef value_compare;
 
-			typedef	alloc	allocator_type;
-			typedef allocator_type::reference 		reference;
-			typedef allocator_type::const_reference const_reference;
-			typedef allocator_type::pointer 		pointer;
-			typedef allocator_type::const_pointer 	const_pointer;
+			typedef	Alloc	allocator_type;
+			typedef typename allocator_type::reference 		reference;
+			typedef typename allocator_type::const_reference const_reference;
+			typedef typename allocator_type::pointer 		pointer;
+			typedef typename allocator_type::const_pointer 	const_pointer;
 
-			typedef Map_iterator 								iterator;
-			typedef Const_Map_iterator 							const_iterator;
-			typedef ft::reverser_iterator<Map_iterator> 		reverse_iterator;
-			typedef ft::reverser_iterator<Const_Map_iterator> 	const_reverse_iterator;
+			typedef map_iterator<value_type>					iterator;
+			typedef const_map_iterator<value_type>				const_iterator;
+			typedef ft::reverse_iterator<iterator> 		reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator> 	const_reverse_iterator;
 
 			typedef std::ptrdiff_t 						difference_type;
 			typedef size_t								size_type;
@@ -75,8 +85,67 @@ namespace ft
 			size_type max_size() const;
 
 			/*
-				** Element acces
+				** Element access
 			*/
+			mapped_type & operator[](const key_type & k);
+
+			/*
+				** Modifiers
+			*/
+			
+			//insert
+			std::pair<iterator, bool> insert(const value_type& val);
+			iterator insert(iterator pos, const value_type& val);
+			
+			template<class InputIt>
+			void insert(InputIt first, InputIt last);
+			
+			//erase
+			void erase(iterator position);
+			size_type erase(const key_type& k);
+			void erase(iterator first, iterator last);
+
+			//swap
+			void swap(map& x);
+
+			//clear
+			void clear(void);
+
+			/*
+				** Observers
+			*/
+
+			key_compare key_comp(void) const;
+			value_compare value_comp(void) const;
+
+			/*
+				** Operations
+			*/
+
+			//find
+			iterator find(const key_type& k);
+			const_iterator find(const key_type& k) const;
+
+			// count
+			size_type count (const key_type& k) const;
+
+			//lower_bound
+			iterator lower_bound(const key_type& k);
+			const_iterator lower_bound(const key_type& k) const;
+
+			//upper_bound
+			iterator upper_bound(const key_type& k);
+			const_iterator upper_bound(const key_type& k) const;
+
+			//equal_range
+			std::pair<iterator, iterator> equal_range(const key_type& k);
+			std::pair<const_iterator, const_iterator> equal_range(const key_type& k) const;
+
+			/*
+				** Allocator
+			*/
+
+			allocator_type get_allocator(void) const;
 
 	};
 	
